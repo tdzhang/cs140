039544c8c216a56cd7036d3e060414333e0dca13
Tongda Zhang <tdzhang@corn06.stanford.edu>
2014-01-14 09:39:48 -0800

    bug fixed

--- src/devices/timer.h
+++ src/devices/timer.h
@@ -28,7 +28,7 @@ void timer_print_stats (void);
 
 /*self defined func*/
 
-/* wake up thread in sleep_list and update sleep_list */mak
+/* wake up thread in sleep_list and update sleep_list */
 void sleep_list_update (void);
 
 #endif /* devices/timer.h */
