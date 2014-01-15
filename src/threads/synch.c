117400d6b0714bfa969a3336aed94e2768644e61
Tongda <tongda.zhang@gmail.com>
2014-01-15 14:16:42 -0800

    try

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -201,7 +201,7 @@ lock_acquire (struct lock *lock)
   ASSERT (!lock_held_by_current_thread (lock));
 
   if(sema_try_down(&lock->semaphore)){
-	  /*printf(">>>>**>>1 lock_acqure: before set lock->holder = thread_current ()");*/
+	  printf(">>>>**>>1 lock_acqure: before set lock->holder = thread_current ()");
 	  lock->holder = thread_current ();
 	  /*printf(">>>>**>>1 lock_acqure: after set lock->holder = thread_current () %d",lock->holder->tid);*/
   }
