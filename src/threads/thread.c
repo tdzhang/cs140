bebdfdaf29be904c85b25c78a196db1171b68406
stevenhan2012 <binghan87@gmail.com>
2014-01-16 00:20:57 -0500

    decide if ready_list is empty before finding the max priority thread

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -353,8 +353,10 @@ thread_set_priority (int new_priority)
   cur->priority = new_priority;
   // TODO: new_priority < cur->actual_priority, find support from donation
   thread_set_actual_priority(cur, new_priority);
-  if (find_max_priority_thread()->actual_priority > cur->actual_priority) {
-	  thread_yield();
+  if (!is_ready_list_empty()) {
+	  if (find_max_priority_thread()->actual_priority > cur->actual_priority) {
+		  thread_yield();
+	  }
   }
   intr_set_level (old_level);
 }
