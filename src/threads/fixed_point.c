3597ed0635fafc2a30d144c483f35afc53799c01
Tongda <tongda.zhang@gmail.com>
2014-01-16 22:51:00 -0800

    add fixed_pont.c.h and changed the makefile

---
+++ src/threads/fixed_point.c
@@ -0,0 +1,78 @@
+/* The main thread acquires a lock.  Then it creates two
+   higher-priority threads that block acquiring the lock, causing
+   them to donate their priorities to the main thread.  When the
+   main thread releases the lock, the other threads should
+   acquire it in priority order.
+
+   Based on a test originally submitted for Stanford's CS 140 in
+   winter 1999 by Matt Franklin <startled@leland.stanford.edu>,
+   Greg Hutchins <gmh@leland.stanford.edu>, Yu Ping Hu
+   <yph@cs.stanford.edu>.  Modified by arens. */
+
+#include <stdio.h>
+
+/*pow(2,14)*/
+#define FIXED_POINT_F 16384
+
+/* Convert n to fixed point n * f  */
+inline int32_t int2f(int32_t n){
+	return n*FIXED_POINT_F;
+}
+
+/* Convert x to integer (rounding toward zero) x / f*/
+inline int32_t f2int_r20(int32_t x){
+	return x/FIXED_POINT_F;
+}
+
+
+/* Convert x to integer (rounding to nearest)  (x + f / 2) / f if x >= 0, (x - f / 2) / f if x <= 0.*/
+inline int32_t f2int_r2near(int32_t x){
+	if(x>0){
+		return (x+FIXED_POINT_F/2)/FIXED_POINT_F;
+	}
+	else{
+		return (x-FIXED_POINT_F/2)/FIXED_POINT_F;
+	}
+}
+
+/* Add x and y: x + y*/
+inline int32_t f_add_f(int32_t x, int32_t y){
+	return x+y;
+}
+
+
+/* Subtract y from x: x - y*/
+inline int32_t f_sub_f (int32_t x, int32_t y) {
+	return x - y;
+}
+
+
+/* Add x and n: x + n * f*/
+inline int32_t f_add_int (int32_t x, int32_t n) {
+	return x + n * FIXED_POINT_F;
+}
+
+/* Subtract n from x: x - n * f */
+inline int32_t f_sub_int (int32_t x, int32_t n) {
+	return x - n * FIXED_POINT_F;
+}
+/* Multiply x by y: ((int64_t) x) * y / f */
+
+inline int32_t f_multiply_f (int32_t x, int32_t y) {
+	return ((int64_t) x) * y / FIXED_POINT_F;
+}
+/* Multiply x by n: x * n */
+inline int32_t f_multiply_int (int32_t x, int32_t n) {
+	return x * n;
+}
+/* Divide x by y:((int64_t) x) * f / y */
+inline int32_t f_divide_f (int32_t x, int32_t y) {
+	ASSERT (y != 0);
+
+	return ((int64_t) x) * FIXED_POINT_F / y;
+}
+/* Divide x by n: x / n */
+inline int32_t f_divide_int (int32_t x, int32_t n) {
+	ASSERT (n != 0);
+	return x / n;
+}
---
+++ src/threads/fixed_point.h
@@ -0,0 +1,31 @@
+#ifndef FIXED_POINT_H
+#define FIXED_POINT_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/* Convert n to fixed point n * f  */
+inline int32_t int2f(int32_t n);
+/* Convert x to integer (rounding toward zero) x / f*/
+inline int32_t f2int_r20(int32_t x);
+/* Convert x to integer (rounding to nearest)  (x + f / 2) / f if x >= 0, (x - f / 2) / f if x <= 0.*/
+inline int32_t f2int_r2near(int32_t x);
+/* Add x and y: x + y*/
+inline int32_t f_add_f(int32_t x, int32_t y);
+/* Subtract y from x: x - y*/
+inline int32_t f_sub_f (int32_t x, int32_t y);
+/* Add x and n: x + n * f*/
+inline int32_t f_add_int (int32_t x, int32_t n);
+/* Subtract n from x: x - n * f */
+inline int32_t f_sub_int (int32_t x, int32_t n);
+/* Multiply x by y: ((int64_t) x) * y / f */
+inline int32_t f_multiply_f (int32_t x, int32_t y);
+/* Multiply x by n: x * n */
+inline int32_t f_multiply_int (int32_t x, int32_t n);
+/* Divide x by y:((int64_t) x) * f / y */
+inline int32_t f_divide_f (int32_t x, int32_t y);
+/* Divide x by n: x / n */
+inline int32_t f_divide_int (int32_t x, int32_t n);
+
+
+#endif /* threads/synch.h */
