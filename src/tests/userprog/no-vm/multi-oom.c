c98802209d8e6abfa4c39a86d090cd4a78502876
Tongda <tongda.zhang@gmail.com>
2014-02-01 16:01:13 -0600

    change test case

--- src/tests/userprog/no-vm/multi-oom.c
+++ src/tests/userprog/no-vm/multi-oom.c
@@ -39,6 +39,7 @@ spawn_child (int c, enum child_termination_mode mode)
   char child_cmd[128];
   snprintf (child_cmd, sizeof child_cmd,
             "%s %d %s", test_name, c, mode == CRASH ? "-k" : "");
+  printf(">>>===>>>create %s",child_cmd);
   return exec (child_cmd);
 }
 
