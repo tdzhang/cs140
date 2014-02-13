8f2b514972d9cfde883fb9dae9ad3617a5297ecd
stevenhan2012 <binghan87@gmail.com>
2014-02-12 22:07:21 -0800

    bug fix

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -18,6 +18,7 @@
 #endif
 /*added for VM*/
 #include "vm/page.h"
+#include "vm/page.c"
 
 /* Random value for struct thread's `magic' member.
    Used to detect stack overflow.  See the big comment at the top
@@ -176,21 +177,6 @@ thread_print_stats (void)
 {
   printf ("Thread: %lld idle ticks, %lld kernel ticks, %lld user ticks\n",
           idle_ticks, kernel_ticks, user_ticks);
-}
-
-
-/*hash function for supplemental page table (a hash table)*/
-unsigned hash_spte1 (const struct hash_elem *e, void *aux UNUSED) {
-  struct supplemental_pte *spte = hash_entry (e, struct supplemental_pte, elem);
-  return hash_int ((int)spte->uaddr);
-}
-
-/*hash less function for supplemental page table (a hash table)*/
-bool hash_less_spte1 (const struct hash_elem *a, const struct hash_elem *b,
-		void *aux UNUSED) {
-  struct supplemental_pte *spte1 = hash_entry (a, struct supplemental_pte, elem);
-  struct supplemental_pte *spte2 = hash_entry (b, struct supplemental_pte, elem);
-  return (spte1->uaddr < spte2->uaddr);
 }
 
 
