91cf2caa53907026be7437211b137bee8253cfe2
stevenhan2012 <binghan87@gmail.com>
2014-02-12 21:40:58 -0800

    bug fix

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -178,6 +178,22 @@ thread_print_stats (void)
           idle_ticks, kernel_ticks, user_ticks);
 }
 
+
+/*hash function for supplemental page table (a hash table)*/
+unsigned hash_spte1 (const struct hash_elem *e, void *aux UNUSED) {
+  struct supplemental_pte *spte = hash_entry (e, struct supplemental_pte, elem);
+  return hash_int ((int)spte->uaddr);
+}
+
+/*hash less function for supplemental page table (a hash table)*/
+bool hash_less_spte1 (const struct hash_elem *a, const struct hash_elem *b,
+		void *aux UNUSED) {
+  struct supplemental_pte *spte1 = hash_entry (a, struct supplemental_pte, elem);
+  struct supplemental_pte *spte2 = hash_entry (b, struct supplemental_pte, elem);
+  return (spte1->uaddr < spte2->uaddr);
+}
+
+
 /* Creates a new kernel thread named NAME with the given initial
    PRIORITY, which executes FUNCTION passing AUX as the argument,
    and adds it to the ready queue.  Returns the thread identifier
@@ -274,22 +290,6 @@ thread_create (const char *name, int priority,
   }
   return tid;
 }
-
-
-/*hash function for supplemental page table (a hash table)*/
-unsigned hash_spte1 (const struct hash_elem *e, void *aux UNUSED) {
-  struct supplemental_pte *spte = hash_entry (e, struct supplemental_pte, elem);
-  return hash_int ((int)spte->uaddr);
-}
-
-/*hash less function for supplemental page table (a hash table)*/
-bool hash_less_spte1 (const struct hash_elem *a, const struct hash_elem *b,
-		void *aux UNUSED) {
-  struct supplemental_pte *spte1 = hash_entry (a, struct supplemental_pte, elem);
-  struct supplemental_pte *spte2 = hash_entry (b, struct supplemental_pte, elem);
-  return (spte1->uaddr < spte2->uaddr);
-}
-
 
 
 /* Puts the current thread to sleep.  It will not be scheduled
