cea740b400b1b5b5d7d8bed833058a0d19606c2f
stevenhan2012 <binghan87@gmail.com>
2014-02-19 19:42:56 -0800

    bf

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -16,9 +16,8 @@
 #ifdef USERPROG
 #include "userprog/process.h"
 #endif
-#ifdef VM
 #include "vm/page.h"
-#endif
+
 
 /* Random value for struct thread's `magic' member.
    Used to detect stack overflow.  See the big comment at the top
