d2d13e7b590e13beb7b857346660bd02f068511d
stevenhan2012 <binghan87@gmail.com>
2014-02-27 00:09:31 -0800

    init swap in the end of init.c

--- src/threads/init.c
+++ src/threads/init.c
@@ -123,10 +123,6 @@ main (void)
   serial_init_queue ();
   timer_calibrate ();
 
-#ifdef VM
-  frame_table_init();
-  swap_pool_init();
-#endif
 
 #ifdef FILESYS
   /* Initialize file system. */
@@ -134,6 +130,13 @@ main (void)
   locate_block_devices ();
   filesys_init (format_filesys);
 #endif
+
+
+#ifdef VM
+  frame_table_init();
+  swap_pool_init();
+#endif
+
 
   printf ("Boot complete.\n");
   
