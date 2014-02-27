1e10bab73c65193c065ce8d9364115ef289be3e8
stevenhan2012 <binghan87@gmail.com>
2014-02-27 09:12:57 -0800

    test

--- src/lib/string.c
+++ src/lib/string.c
@@ -290,7 +290,7 @@ memset (void *dst_, int value, size_t size)
   while (size-- > 0) {
 
 	  i++;
-	  if (i % 1024 == 0 || i > 4090) {
+	  if (i % 1024 == 0) {
 		  printf("==size = %d==\n", size);
 		  printf("======i = %d=====\n", i);
 		  printf("======dst = %u=====\n", (unsigned)dst);
