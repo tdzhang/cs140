f649b292d35415fcfccafff71f1ebb326846f5c3
Tongda <tongda.zhang@gmail.com>
2014-02-26 23:50:56 -0800

    wuyu

--- src/vm/swap.h
+++ src/vm/swap.h
@@ -5,13 +5,15 @@
 #include <stdint.h>
 #include <list.h>
 
+void swap_pool_init();
+void swap_out(struct frame_table_entry *fte);
+void swap_in(struct frame_table_entry *fte, struct swap_page_block *spb);
+
 struct swap_page_block {
 	block_sector_t block_sector_head;
 	struct list_elem elem;
 };
 
-void swap_pool_init();
-/*void swap_out(struct frame_table_entry *fte);*/
-/*void swap_in(struct frame_table_entry *fte, struct swap_page_block *spb);*/
+
 
 #endif /* vm/swap.h */
