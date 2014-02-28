2d831de651b781af29036f95500810420aae8746
stevenhan2012 <binghan87@gmail.com>
2014-02-27 21:39:14 -0800

    test

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -1001,7 +1001,7 @@ void process_vm_clean(){
 	  }
 
 	  /* clean up supplemental page table and swap */
-	  hash_destroy (supplemental_pt, spt_clean_up_func);
+	  /*hash_destroy (supplemental_pt, spt_clean_up_func);*/
 }
 
 
