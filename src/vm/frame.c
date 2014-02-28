ae1527d72fc3f5278441e1747713bc80a82d5543
Tongda <tongda.zhang@gmail.com>
2014-02-27 23:26:26 -0800

    minor

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -162,7 +162,10 @@ free_fte (struct frame_table_entry *fte)
   /*clean up*/
 
   list_remove (&fte->elem);
-  palloc_free_page(fte->frame_addr);
+  if(fte->spte->type_code!=SPTE_STACK_INIT){
+	  palloc_free_page(fte->frame_addr);
+  }
+
   free(fte);
 
   lock_release (&frame_table_lock);
