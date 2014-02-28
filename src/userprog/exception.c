4ca9d08b10013efce5d26a0ec896f24cd23f093f
Tongda <tongda.zhang@gmail.com>
2014-02-28 09:31:23 -0800

    diable inter in page fault

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -18,7 +18,6 @@ static long long page_fault_cnt;
 
 static void kill (struct intr_frame *);
 static void page_fault (struct intr_frame *);
-static struct lock global_page_fault_lock;
 
 /* Registers handlers for interrupts that can be caused by user
    programs.
@@ -67,8 +66,6 @@ exception_init (void)
      We need to disable interrupts for page faults because the
      fault address is stored in CR2 and needs to be preserved. */
   intr_register_int (14, 0, INTR_OFF, page_fault, "#PF Page-Fault Exception");
-
-  lock_init(&global_page_fault_lock);
 }
 
 /* Prints exception statistics. */
@@ -142,13 +139,6 @@ page_fault (struct intr_frame *f)
   void *fault_addr;  /* Fault address. */
   void *esp;  /*get esp address*/
 
-  bool already_hold_lock=false;
-  if(!lock_held_by_current_thread (&global_page_fault_lock)){
-	  lock_acquire(&global_page_fault_lock);
-  }else{
-	  already_hold_lock=true;
-  }
-
 
   /* Obtain faulting address, the virtual address that was
      accessed to cause the fault.  It may point to code or to
@@ -191,17 +181,17 @@ page_fault (struct intr_frame *f)
 
    if (not_present)
    {
+	   enum intr_level old_level;
+	   old_level = intr_disable ();
+
+
 	   ASSERT (!lock_held_by_current_thread (&filesys_lock) && 19==19 );
 	   /*try to load the page, if success return*/
 	   if(try_load_page(fault_addr)){
 		   if (holding_filesys_lock) {
 		   	  lock_acquire(&filesys_lock);
 		   }
-
-		   if(!already_hold_lock){
-			   lock_release(&global_page_fault_lock);
-		   }
-
+		   intr_set_level (old_level);
 		   return;
 	   }
 
@@ -216,13 +206,12 @@ page_fault (struct intr_frame *f)
 				   if (holding_filesys_lock) {
 				   		   	  lock_acquire(&filesys_lock);
 				   }
-				   if(!already_hold_lock){
-				   			   lock_release(&global_page_fault_lock);
-				   }
+				   intr_set_level (old_level);
 				   return;
 			   }
 		   }
 	   }
+	   intr_set_level (old_level);
    }
 
     /*if this is a system_call from a user thread, terminate it with kernel intact*/
@@ -230,18 +219,12 @@ page_fault (struct intr_frame *f)
 		if (holding_filesys_lock) {
 			lock_acquire(&filesys_lock);
 		}
-		if(!already_hold_lock){
-					   lock_release(&global_page_fault_lock);
-		}
 		 user_exit(-1);
 	}
 	/*if this is  a user call, terminate it with kill()*/
 	else if (user){
 		if (holding_filesys_lock) {
 			lock_acquire(&filesys_lock);
-		}
-		if(!already_hold_lock){
-					   lock_release(&global_page_fault_lock);
 		}
 		user_exit(-1);
 	}
@@ -257,9 +240,6 @@ page_fault (struct intr_frame *f)
 			  user ? "user" : "kernel");
 	  if (holding_filesys_lock) {
 		lock_acquire(&filesys_lock);
-	  }
-	  if(!already_hold_lock){
-	  		lock_release(&global_page_fault_lock);
 	  }
 	  kill (f);
 	}
