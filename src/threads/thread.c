304e9b59ec6a3ab49cab7f492484c35fc4ec7d31
Tongda <tongda.zhang@gmail.com>
2014-02-27 22:20:18 -0800

    test2

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -976,7 +976,7 @@ void spt_clean_up_func (struct hash_elem *e, void *aux) {
 
 	if (fte != NULL) {
 
-		pagedir_clear_page(&cur->pagedir,spte->uaddr);
+		pagedir_clear_page(&fte->t->pagedir,spte->uaddr);
 		/*
 		free_fte(fte);
 		*/
