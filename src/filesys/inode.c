edf09e6897b7e5c883dcba84388e1d371a17d9ca
stevenhan2012 <binghan87@gmail.com>
2014-03-06 11:29:37 -0800

    add ASSERT (sector_idx != INVALID_SECTOR_ID); in inode_read_at

--- src/filesys/inode.c
+++ src/filesys/inode.c
@@ -301,6 +301,8 @@ inode_read_at (struct inode *inode, void *buffer_, off_t size, off_t offset)
     {
       /* Disk sector to read, starting byte offset within sector. */
       block_sector_t sector_idx = byte_to_sector (inode, offset);
+      ASSERT (sector_idx != INVALID_SECTOR_ID);
+
       int sector_ofs = offset % BLOCK_SECTOR_SIZE;
 
       /* Bytes left in inode, bytes left in sector, lesser of the two. */
