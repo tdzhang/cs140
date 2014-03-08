db867c62a4b7b34fd1db36a2a2dc23203857c8bf
Tongda <tongda.zhang@gmail.com>
2014-03-07 20:28:57 -0800

    bf

--- src/filesys/directory.h
+++ src/filesys/directory.h
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include "devices/block.h"
+#include "filesys/off_t.h"
 
 /* Maximum length of a file name component.
    This is the traditional UNIX maximum length.
