ae9898968db0f35184172bb408a43e95c81fef4d
Tongda <tongda.zhang@gmail.com>
2014-03-07 20:24:38 -0800

    add inode_create_dir (block_sector_t sector, off_t count)

--- src/filesys/directory.h
+++ src/filesys/directory.h
@@ -27,4 +27,20 @@ bool dir_add (struct dir *, const char *name, block_sector_t);
 bool dir_remove (struct dir *, const char *name);
 bool dir_readdir (struct dir *, char name[NAME_MAX + 1]);
 
+/* A directory. */
+struct dir
+  {
+    struct inode *inode;                /* Backing store. */
+    off_t pos;                          /* Current position. */
+  };
+
+/* A single directory entry. */
+struct dir_entry
+  {
+    block_sector_t inode_sector;        /* Sector number of header. */
+    bool is_dir;					/*if the entry is a directory*/
+    char name[NAME_MAX + 1];            /* Null terminated file name. */
+    bool in_use;                        /* In use or free? */
+  };
+
 #endif /* filesys/directory.h */
--- src/filesys/filesys.c
+++ src/filesys/filesys.c
@@ -104,6 +104,7 @@ do_format (void)
 {
   printf ("Formatting file system...");
   free_map_create ();
+  //TODO: to be deleted or defined
   if (!dir_create (ROOT_DIR_SECTOR, 16))
     PANIC ("root directory creation failed");
   free_map_close ();
