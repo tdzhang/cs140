d41274ce5d2df81eb0939a2a6e26a49f968767b5
stevenhan2012 <binghan87@gmail.com>
2014-03-09 15:15:48 -0700

    bf

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,7 +1,6 @@
 #include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
-#include <syscall.h>
 #include "lib/user/syscall.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
