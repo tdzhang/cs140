0ab14d50d1d0c2c0a83536f9086b141f6f56910b
stevenhan2012 <binghan87@gmail.com>
2014-03-13 14:21:07 -0700

    comment intr_enable in flushout

--- src/filesys/cache.c
+++ src/filesys/cache.c
@@ -206,8 +206,7 @@ bool flush_cache_entry(int entry_index, bool need_wait) {
 		}
 		buffer_cache[entry_index].flushing_out = true;
 		lock_release(&buffer_cache[entry_index].lock);
-		//TODO: release all lock held
-		intr_enable ();
+		/*intr_enable ();*/
 		block_write(fs_device, buffer_cache[entry_index].sector_id, buffer_cache[entry_index].sector_data);
 		lock_acquire(&buffer_cache[entry_index].lock);
 		buffer_cache[entry_index].dirty = false;
